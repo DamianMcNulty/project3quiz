b864126fed5018c0f6839296ee917ac9735cd836
Damian McNulty <damianmcnulty@me.com>
2018-12-08 12:32:41 +0000

    game

--- runserver.py
+++ runserver.py
@@ -1,16 +1,72 @@
-# import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from os import environ
+import json
+from datetime import datetime
 
-# create the application object
 app = Flask(__name__)
 
-# use decorators to link the function to a url
-@app.route('/hello')
-def home():
-    return "Hello, World!"  # return a string
+app.secret_key = '7l8/%Zb'
 
-# start the server with the 'run()' method
+names = []
+user = []
+
+score = 0
+question = 0
+users = []
+
+@app.route('/hello')
+def hello():
+    return "Hello, World!"
+
+@app.route('/', methods=["GET", "POST"])
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    if request.method == "POST":
+        if request.form["name"] in names:
+            message = "The user name " + request.form["name"] + " has already been taken"
+            flash(message)
+            return render_template('index.html', title='Home Page', year=datetime.now().year)
+        else:
+            names.append(request.form["name"])
+        return redirect(request.form["name"])
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/<name>', methods=["GET", "POST"])
+def game(name):
+    data = []
+    with open("data/data.json", "r") as json_data:
+        data = json.load(json_data)
+    global question
+    global score
+    if request.method == "POST" and request.form["answer"] == data[question]['answer']:
+        score += 1
+        question += 1
+        user.append({"name": name, "score": score})
+        return redirect('/' + name)
+    if request.method == "POST" and request.form["answer"] != data[question]['answer']:
+        message = "You guessed: " + request.form["answer"]
+        flash(message)
+        return redirect('/' + name)
+    return render_template("game.html", page_title = "Java Quiz", data=data[question], name=name, score=score, question=question, year=datetime.now().year)
+
+
+@app.route('/leaderboard', methods=["GET"])
+def leaderboard():
+    """Renders the leaderboard."""
+    global user
+    return render_template(
+        'leaderboard.html',
+        title='Leaderboard',
+        year=datetime.now().year,
+        message='Leaderboard', 
+        scores = user
+    )
+
 if __name__ == '__main__':
     HOST = environ.get('IP')
     PORT = int(environ.get('PORT'))
--- test.py
+++ test.py
@@ -10,11 +10,87 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/hello', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         
-    # / contains 'Hello, World!'
+    # /hello contains 'Hello, World!'
     def test_hello_name(self):
         tester = app.test_client(self)
         response = tester.get('/hello', content_type='html/text')
         self.assertTrue(b'Hello, World!', response.data)
+    
+     # Ensure that flask was set up correctly
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        
+    # index.html contains 'Java Quiz'
+    def test_index_name(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertTrue(b'Java Quiz', response.data)
+        
+    # Given a user called f1 the user can play the game
+    def test_index_username_login(self):
+        tester = app.test_client(self)
+        response = tester.post(
+        '/',
+        data=dict(name="f1"),
+        follow_redirects=True
+        )
+        self.assertIn(b'Given the following code:', response.data)
+    
+        
+    # Given a user called f1, another user called f1 can't play the game
+    def test_index_username_login_again(self):
+        tester = app.test_client(self)
+        response = tester.post(
+        '/',
+        data=dict(name="f1"),
+        follow_redirects=True
+        )
+        response = tester.post(
+        '/',
+        data=dict(name="f1"),
+        follow_redirects=True
+        )
+        self.assertIn(b'The user name f1 has already been taken', response.data)
+        
+    # leaderboard.html contains 'Leaderboard'
+    def test_leaderboard_name(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertTrue(b'Leaderboard', response.data)
+    
+    # leaderboard route works ok
+    def test_leaderboard(self):
+        tester = app.test_client(self)
+        response = tester.get('/leaderboard', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+    # user f1 appears in leaderboard
+    def test_leaderboard_user(self):
+        tester = app.test_client(self)
+        response = tester.post(
+        '/',
+        data=dict(name="f1"),
+        follow_redirects=True
+        )
+        self.assertIn(b'f1', response.data)
+        
+    # game.html works ok
+    def test_game(self):
+        tester = app.test_client(self)
+        response = tester.get('/game', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+     # Given a user called f1 the user can play the game
+    def test_game_question_num(self):
+        tester = app.test_client(self)
+        response = tester.post(
+        '/',
+        data=dict(name="f2"),
+        follow_redirects=True
+        )
+        self.assertIn(b'question 1', response.data)
 
 if __name__ == '__main__':
     unittest.main()
