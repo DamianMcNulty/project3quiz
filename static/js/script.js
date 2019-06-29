343c558c6bf958ceee12cb3bf25f44de6703c4c7
Damian McNulty <damianmcnulty@me.com>
2019-06-29 18:18:46 +0100

    added datatables.net functionality to the leaderboard

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+    $('[data-toggle="tooltip"]').tooltip();
+    $('.leaderboard').DataTable( {
+        fixedHeader: true,
+        responsive: true,
+        "iDisplayLength": 5,
+        "aLengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+        "order": [],
+        "columnDefs": [ {
+        "targets"  : 'no-sort',
+        "orderable": false,
+        }]
+   });
 });
