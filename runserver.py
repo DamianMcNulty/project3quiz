751147689bea8ed166a8a34db8109036ed17e2c7
Damian McNulty <DamianMcNulty@users.noreply.github.com>
2022-04-22 21:03:16 +0100

    Quiz-2 next button

--- runserver.py
+++ runserver.py
@@ -78,18 +78,19 @@ def game(name):
     if 'user' not in session:
         return redirect('/')
     if request.method == "POST":
-        if(session['question'] == number_of_questions):
-            session['gameover'] = True
-        session['question'] += 1
-        return redirect('/user/' + name)
-        if request.form["answer"] != session['data'][session['question']]['answer']:
-            message = "Answer " + request.form["answer"] + " is incorrect, please try again."
-            flash(message)
-            return redirect('/user/' + name)
-        if request.form["answer"] == session['data'][session['question']]['answer']:
-            session['score'] += 1
+        if request.form["skip"] == "Next":
+            if(session['question'] == number_of_questions):
+                session['gameover'] = True
             session['question'] += 1
             return redirect('/user/' + name)
+            if request.form["answer"] != session['data'][session['question']]['answer']:
+                message = "Answer " + request.form["answer"] + " is incorrect, please try again."
+                flash(message)
+                return redirect('/user/' + name)
+            if request.form["answer"] == session['data'][session['question']]['answer']:
+                session['score'] += 1
+                session['question'] += 1
+                return redirect('/user/' + name)
     return render_template("game.html", title = "Game Over" if session['gameover'] else "Question " + str(session['question'] + 1), data = session['data'][session['question']], question = session['question'], year=datetime.now().year)
 
 
