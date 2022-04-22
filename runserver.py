4e03d367ba6c6b67e79a0c8c110d8c1e575ba59a
Damian McNulty <DamianMcNulty@users.noreply.github.com>
2022-04-22 01:07:22 +0100

    Quiz-2 check answer

--- runserver.py
+++ runserver.py
@@ -78,13 +78,12 @@ def game(name):
     if 'user' not in session:
         return redirect('/')
     if request.method == "POST":
-        if request.form["skip"] == "Next":
-            if request.form["answer"] == session['data'][session['question']]['answer']:
-                session['score'] += 1
-                session['question'] += 1
-                if(session['question'] == number_of_questions):
-                session['gameover'] = True
-                return redirect('/user/' + name)
+        if request.form["answer"] == session['data'][session['question']]['answer']:
+            session['score'] += 1
+            session['question'] += 1
+            if(session['question'] == number_of_questions):
+            session['gameover'] = True
+            return redirect('/user/' + name)
     return render_template("game.html", title = "Game Over" if session['gameover'] else "Question " + str(session['question'] + 1), data = session['data'][session['question']], question = session['question'], year=datetime.now().year)
 
 
